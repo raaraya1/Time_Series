d63321cdaaab6e5397c0b9a76304dca0798b3d0a
raaraya1 <raaraya1@miuandes.cl>
2022-01-27 21:35:53 -0300

    install scip

--- setup.sh
+++ setup.sh
@@ -7,3 +7,12 @@ enableCORS = false\n\
 headless = true\n\
 \n\
 " > ~/.streamlit/config.toml
+
+tar xvzf scip-x.y.z.tgz                                                       # unpack the tarball
+cd scip-x.y.z                                                                 # change into the directory
+mkdir build                                                                   # create a new directory
+cd build                                                                      # change directories
+cmake .. -DCMAKE_INSTALL_PREFIX=<install/dir> [-DSOPLEX_DIR=/path/to/soplex]  # configure the build
+make                                                                          # start compiling SCIP
+make check                                                                    # (recommended) check build
+make install                                                                  # (optional) install SCIP executable, library, and headers
