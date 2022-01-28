1a3fcc0969e9986c78d07692305bca6e586a148a
raaraya1 <raaraya1@miuandes.cl>
2022-01-28 12:14:03 -0300

    install scip

--- setup.sh
+++ setup.sh
@@ -8,14 +8,4 @@ headless = true\n\
 \n\
 " > ~/.streamlit/config.toml
 
-apt-get update
-
-apt-get install build-essential libreadline-dev libz-dev libgmp3-dev lib32ncurses5-dev libboost-program-options-dev libblas-dev
-
-tar xvzf scipoptsuite-7.0.2.tgz
-cd scipoptsuite-7.0.2
-
-apt-get install make
-make install
-
-make gcg
+python setup.py install
